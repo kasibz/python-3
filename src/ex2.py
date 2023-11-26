c20c3c769704a044eb856793554b814b93ca771c
kasib <kasibzxc@gmail.com>
2023-11-26 17:34:24 -0500

    solved ex2

--- src/ex2.py
+++ src/ex2.py
@@ -1,5 +1,23 @@
+import csv
+import os
+import numpy as np
 
 def find_total_visits():
+    total = 0
+    for filename in os.listdir("../files/"):
+        if filename.endswith(".csv"):
+            file_path = os.path.join("../files/", filename)
+
+            # read csv
+            with open(file_path, "r") as csvfile:
+                for row in csv.reader(csvfile):
+                    for item in row:
+                        try:
+                            total += int(item)
+                        except:
+                            pass
+            
+    return total
     pass # TODO
 
 
