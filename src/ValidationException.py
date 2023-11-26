43b4d0bfa0316920d3529e1ab06d183d07e36cff
kasib <kasibzxc@gmail.com>
2023-11-26 00:27:41 -0500

    solved exercise 1

--- src/ValidationException.py
+++ src/ValidationException.py
@@ -1,4 +1,4 @@
-class ValidationException(Exception):
+class ValidationException(BaseException):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
--- src/ex1.py
+++ src/ex1.py
@@ -1,9 +1,19 @@
+import csv
 from ValidationException import ValidationException
 
 
-def validate_file():
-    pass # TODO
+def validate_file(input_file):
+    lines = []
+    with open(f"../files/{input_file}", "r") as file:
+        lines = file.readlines()
 
+    for line in lines[1:]:
+        line_lst = line.split(", ")
+        checked_num = line_lst[1]
+        try:
+            res = int(checked_num)
+        except:
+            raise ValidationException(f"Invalid mileage:  {checked_num}")
 
 
 def ex1():
